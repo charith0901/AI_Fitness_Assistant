8aebe8f48e3dd751cdd95a877367f1bf616eef74
charith0901 <charithjayasankha@gmail.com>
2025-05-20 22:22:53 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=["http://localhost:5173","https://ai-fitness-assistant-273p.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
